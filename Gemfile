22e789868df488482f6bb6d8e762d005c8ac4b66
Pete Hamilton <peterejhamilton@gmail.com>
2014-11-18 19:04:48 +0000

    Implemented example project

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
 gem "sinatra"
+gem "rest_client"
--- app.rb
+++ app.rb
@@ -1,44 +1,74 @@
 require 'rubygems'
 require 'sinatra'
+require 'rest_client'
+require 'base64'
+require 'securerandom'
+require 'json'
 
 configure do
   enable :sessions
 end
+set :session_secret, 'IMASECRETYAY'
 
-helpers do
-  def username
-    session[:identity] ? session[:identity] : 'Hello stranger'
-  end
+CREDITOR_ID = ENV['CREDITOR_ID']
+API_KEY_ID = ENV['API_KEY_ID']
+API_KEY_SECRET = ENV['API_KEY_SECRET']
+API_URL = "https://api-staging.gocardless.com"
+HEADERS = {
+  'GoCardless-Version' => '2014-11-03',
+  'Content-Type' => 'application/json'
+}
+
+API = RestClient::Resource.new(API_URL,
+                               user: API_KEY_ID,
+                               password: API_KEY_SECRET,
+                               headers: HEADERS)
+
+# Before every request, make sure they get assigned a session ID.
+before do
+  session[:token] ||= SecureRandom.uuid
 end
 
-before '/secure/*' do
-  if !session[:identity] then
-    session[:previous_url] = request.path
-    @error = 'Sorry guacamole, you need to be logged in to visit ' + request.path
-    halt erb(:login_form)
-  end
-end
-
+# Customer Vists the DVLA
 get '/' do
-  erb 'Can you handle a <a href="/secure/place">secret</a>?'
+  erb :index
 end
 
-get '/login/form' do 
-  erb :login_form
+# DVLA kicks off
+post '/purchase' do
+  package = params[:package]
+  uri = URI.parse(request.env["REQUEST_URI"])
+  success_url = "#{uri.scheme}://#{uri.host}/payment_complete?package=#{package}"
+
+  payload = {
+    redirect_flows: {
+      description: "#{package.capitalize} License - £#{rand(1..5)*50}",
+      session_token: session[:token],
+      success_redirect_url: success_url,
+      links: {
+        creditor: CREDITOR_ID
+      }
+    }
+  }
+
+  response = API['/redirect_flows'].post payload.to_json
+  redirect JSON.parse(response)["redirect_flows"]["redirect_url"]
 end
 
-post '/login/attempt' do
-  session[:identity] = params['username']
-  where_user_came_from = session[:previous_url] || '/'
-  redirect to where_user_came_from 
+# Hit when customer has gone through our flow pages
+get '/payment_complete' do
+  redirect_flow_id = params[:redirect_flow_id]
+
+  payload = {
+    data: {
+      session_token: session[:token]
+    }
+  }
+  API["/redirect_flows/#{redirect_flow_id}/actions/complete"].post payload.to_json
+  redirect "/thankyou?package=#{params[:package]}"
 end
 
-get '/logout' do
-  session.delete(:identity)
-  erb "<div class='alert alert-message'>Logged out</div>"
-end
-
-
-get '/secure/place' do
-  erb "This is a secret place that only <%=session[:identity]%> has access to!"
+get '/thankyou' do
+  @package = params[:package]
+  erb :thankyou
 end
