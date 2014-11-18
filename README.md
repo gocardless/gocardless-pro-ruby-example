d23e650f6de42962c989650e1293caf6b0ef0103
Pete Hamilton <peterejhamilton@gmail.com>
2014-11-18 17:53:10 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+Hello!
+====
+
+This is a demonstrator for [Sinatra](http://www.sinatrarb.com/) and [Twitter Bootstrap](http://getbootstrap.com/). 
+
+Use it as a boilerplate to start your own apps.
+
+Go!
+===
+
+Download and run sinatra-bootstrap:
+
+    git clone https://github.com/bootstrap-ruby/sinatra-bootstrap
+    
+    cd sinatra-bootstrap
+    bundle install             # To install sinatra
+    
+    bundle exec ruby app.rb    # To run the sample
+	
+Then open [http://localhost:4567/](http://localhost:4567/)
+
+What's next?
+============
+- Try the rerun gem to restart Sinatra automatically when you change source files: https://github.com/alexch/rerun
---
+++ app.rb
@@ -0,0 +1,44 @@
+require 'rubygems'
+require 'sinatra'
+
+configure do
+  enable :sessions
+end
+
+helpers do
+  def username
+    session[:identity] ? session[:identity] : 'Hello stranger'
+  end
+end
+
+before '/secure/*' do
+  if !session[:identity] then
+    session[:previous_url] = request.path
+    @error = 'Sorry guacamole, you need to be logged in to visit ' + request.path
+    halt erb(:login_form)
+  end
+end
+
+get '/' do
+  erb 'Can you handle a <a href="/secure/place">secret</a>?'
+end
+
+get '/login/form' do 
+  erb :login_form
+end
+
+post '/login/attempt' do
+  session[:identity] = params['username']
+  where_user_came_from = session[:previous_url] || '/'
+  redirect to where_user_came_from 
+end
+
+get '/logout' do
+  session.delete(:identity)
+  erb "<div class='alert alert-message'>Logged out</div>"
+end
+
+
+get '/secure/place' do
+  erb "This is a secret place that only <%=session[:identity]%> has access to!"
+end
